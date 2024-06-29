6f9d30c91d701dd8a947007b5075465d2511a882
jp3droal <jp5769148@gmail.com>
2024-06-29 14:38:37 -0300

    adição de atividades

---
+++ questao4.js
@@ -0,0 +1,10 @@
+
+let soma = 0
+
+for (let numero = 0; numero < 10; numero++){
+    let novoNumero = Number(prompt("Digite aqui seu numero"))
+    soma += novoNumero 
+ 
+}
+console.log(`a soma é ${soma}`)
+alert(`a soma é ${soma}`)
